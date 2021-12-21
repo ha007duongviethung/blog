3b17cd3701ce919a888cd9e294eb9de48fb6dccb
Việt Hùng Dương <anhhungleonard@gmail.com>
2021-12-21 09:58:38 +0700

    Header page

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,25 @@
 class Main {
+    $ = document.querySelector.bind(document)
+    $$ = document.querySelectorAll.bind(document)
+    btnSearch
+    btnClose
+    inputSearch
+
     constructor() {
         console.log("Hello Heroes Pluss")
     }
-}
 
-const main =  new Main();
+    buttonSearch() {
+        this.btnSearch = this.$('.header-search')
+        this.btnClose = this.$('.header-close')
+        this.inputSearch = this.$('.header-input')
+
+        this.btnSearch.addEventListener('click', () => {
+            this.inputSearch.classList.remove('hidden')
+        })
+
+        this.btnClose.addEventListener('click', () => {
+            this.inputSearch.classList.add('hidden')
+        })
+    }
+}
